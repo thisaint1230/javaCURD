981401775679afe990dadd22d5fce95cd23c2688
thisaint <tjdnftjdgus@naver.com>
2023-09-05 14:12:40 +0900

    create foundation

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        WordManager wordManager = new WordManager();
+        wordManager.Menu();
+    }
+
+}
