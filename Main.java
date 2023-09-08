ee72502577dcf362870a8bd2ee9830b89cd40757
thisaint <tjdnftjdgus@naver.com>
2023-09-08 10:32:43 +0900

    create menu

--- Main.java
+++ Main.java
@@ -7,15 +7,10 @@ public class Main {
         WordManager wordManager = new WordManager();
 
         while(true) {
-            int menu=wordManager.Menu();
-            if(menu==0) break;         //" 0. 나가기 \n "
-            if(menu==1);            //" 1. 모든 단어 보기 \n "
-            if(menu==2);                // " 2. 수준별 단어 보기 \n "
-            if(menu==3);                 //" 3. 단어 검색 \n "
-            if(menu==4);                  //" 4. 단어 추가 \n "
-            if(menu==5);                    //" 5. 단어 수정 \n "
-            if(menu==6);                               //" 6. 단어 삭제 \n "
-            if(menu==7);                              //" 7. 파일 저장 \n "
+            int menu = wordManager.Menu();
+            if (menu == 0) break;         //" 0. 나가기 \n "
+
+        }
 
 
 
--- WordManager.java
+++ WordManager.java
@@ -18,4 +18,6 @@ public class WordManager {
     );
        return menuNum.nextInt();
     }
+
+
 }
