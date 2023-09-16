59c60af9dae8831a2138d1972b3f38b06ea77e82
thisaint <tjdnftjdgus@naver.com>
2023-09-16 21:24:45 +0900

    진짜 끝

--- word/Main.java
+++ word/Main.java
@@ -4,8 +4,6 @@ import word.WordProgram;
 
 public class Main {
     public static void main(String[] args) {
-        String osName = System.getProperty("os.name").toLowerCase();
-        System.out.println("OS name : " + osName);
 
         new WordProgram().program();
     }
